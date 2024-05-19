2ee14a89fa3851240de3327b334405c7c1c7fa4c
BaturHamzaogullari <baturhamzaogullari@gmail.com>
2024-05-19 18:33:42 +0200

    project part 1
    complete version of part 1

--- CDataframe.c
+++ CDataframe.c
@@ -5,6 +5,9 @@
 #include "CDataframe.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+//------------ Supply ---------------
 
 CDataframe create_CDataframe(int size)
 {
@@ -44,20 +47,175 @@ void fill_CDataframe(CDataframe frame)
     }
     printf("All the columns are filled\n");
 }
+//------------------------------------
 
-void display_CDataframe(CDataframe frame, int size)
+
+//------------ Display ---------------
+
+void display_CDataframe(CDataframe frame, int size, int lineLimit, int columnLimit)
 {
-    int max = 0;
-    for (int i = 0; i < size; i++) {
-        if(frame[i]->taille_logique > max) max = frame[i]->taille_logique;
+    printf("    | ");
+    for (int i = 0; i < columnLimit; ++i) {
+        printf("%*s ",10,frame[i]->title);
     }
-    for (int i = 0; i < max; ++i) {
-        for (int j = 0; j < size; ++j) {
+    printf("\n");
+    for (int i = 0; i < lineLimit; ++i) {
+        printf("[%d] |",i);
+        for (int j = 0; j < columnLimit; ++j) {
             if(i<frame[j]->taille_logique)
-                printf("%d ", frame[j]->data[i]);
+            {
+                printf("%*d", 10,frame[j]->data[i]);
+
+            }
             else
-                printf("  ");
+                printf("%*c",10,' ');
         }
         printf("\n");
     }
+}
+
+void display_CDataframe_all(CDataframe frame, int size)
+{
+    int max = 0;
+    for (int i = 0; i < size; i++)
+        if(frame[i]->taille_logique > max) max = frame[i]->taille_logique;
+    display_CDataframe(frame,size, max,size);
+}
+
+void display_CDataframe_LineLimit(CDataframe frame, int size, int lineLimit)
+{
+    int max = 0;
+    for (int i = 0; i < size; i++)
+        if(frame[i]->taille_logique > max) max = frame[i]->taille_logique;
+    if(lineLimit > max)
+        printf("There is not enough lines in this CDataframe, please enter a smaller limit.\n");
+    else
+        display_CDataframe(frame,size, lineLimit,size);
+}
+
+void display_CDataframe_ColumnLimit(CDataframe frame, int size, int ColumnLimit)
+{
+    if(ColumnLimit > size)
+        printf("There is not enough columns in this CDataframe, please enter a smaller limit.\n");
+    else
+    {
+        int max = 0;
+        for (int i = 0; i < size; i++)
+            if(frame[i]->taille_logique > max) max = frame[i]->taille_logique;
+        display_CDataframe(frame,size,max,ColumnLimit);
+    }
+}
+//------------------------------------
+
+//---------- Usual operations ------------
+
+void rename_column_title(CDataframe frame, int size)
+{
+    int index = 0;
+    char newtitle[100];
+    printf("Please enter the index of the column you want to rename: ");
+    scanf("%d", &index);
+
+    if(index > size-1)
+        printf("invalid index");
+    else
+    {
+        printf("Enter the new title: ");
+        scanf("%s",newtitle);
+        if(strlen(newtitle) > strlen(frame[index]->title))
+        {
+            frame[index]->title = (char*)realloc(frame[index]->title,strlen(newtitle) + 1);
+            strcpy(frame[index]->title, newtitle);
+        }
+        else
+            strcpy(frame[index]->title, newtitle);
+    }
+}
+
+void check_existence(CDataframe frame, int size,int value)
+{
+    int check = 1;
+    for (int i = 0; i < size && check; ++i) {
+        for (int j = 0; j < frame[i]->taille_logique; ++j) {
+            if(value == frame[i]->data[j]) {
+                printf("the value %d exist in the CDataframe.\n", value);
+                check = 0;
+                break;
+            }
+        }
+    }
+}
+
+void display_column_names(CDataframe frame, int size)
+{
+    printf("The column names in the CDataframe:\n");
+    for (int i = 0; i < size; ++i) {
+        printf("%d: %s   ", i,frame[i]->title);
+    }
+    printf("\n");
+}
+
+void change_value_at(CDataframe frame, int size)
+{
+    int x = -1, y = -1, newvalue;
+    printf("Enter the coordinates (column then whitespace then ligne) of cell you want to change: ");
+    scanf("%d %d", &x,&y);
+    printf("now enter the new value: ");
+    scanf("%d", &newvalue);
+    frame[x]->data[y] = newvalue;
+    printf("\n");
+}
+
+//--------- Analysis and statistics -----
+
+void display_NumberOfColumns(CDataframe frame, int size)
+{
+    printf("Number of Columns in the CDataframe is %d", size);
+}
+
+void display_NumberOfLines(CDataframe frame, int size)
+{
+    int max = 0;
+    for (int i = 0; i < size; i++)
+        if(frame[i]->taille_logique > max) max = frame[i]->taille_logique;
+    printf("The number of Lines in the CDataframe is %d", max);
+}
+
+void values_equal_to_CD(CDataframe frame, int size, int x)
+{
+    int counter = 0;
+    for (int i = 0; i < size; ++i) {
+        for(int j = 0; j < frame[i]->taille_logique; j++)
+        {
+            if(frame[i]->data[j] == x)
+                counter++;
+        }
+    }
+    printf("%d cells contain the value %d", counter, x);
+}
+
+void values_superior_to_CD(CDataframe frame, int size, int x)
+{
+    int counter = 0;
+    for (int i = 0; i < size; ++i) {
+        for(int j = 0; j < frame[i]->taille_logique; j++)
+        {
+            if(frame[i]->data[j] > x)
+                counter++;
+        }
+    }
+    printf("%d cells contain a higher value than %d", counter, x);
+}
+
+void values_inferior_to_CD(CDataframe frame, int size, int x)
+{
+    int counter = 0;
+    for (int i = 0; i < size; ++i) {
+        for(int j = 0; j < frame[i]->taille_logique; j++)
+        {
+            if(frame[i]->data[j] < x)
+                counter++;
+        }
+    }
+    printf("%d cells contain a smaller value than %d", counter, x);
 }
--- CDataframe.h
+++ CDataframe.h
@@ -9,8 +9,27 @@
 
 typedef column** CDataframe;
 
+//------------ Supply ---------------
 CDataframe create_CDataframe(int size);
 void fill_CDataframe(CDataframe frame);
-void display_CDataframe(CDataframe frame, int size);
+
+//------------ Display ---------------
+void display_CDataframe(CDataframe frame, int size, int lineLimit, int columnLimit);
+void display_CDataframe_all(CDataframe frame, int size);
+void display_CDataframe_LineLimit(CDataframe frame, int size, int lineLimit);
+void display_CDataframe_ColumnLimit(CDataframe frame, int size, int ColumnLimit);
+
+//---------- Usual operations ------------
+void rename_column_title(CDataframe frame, int size);
+void check_existence(CDataframe frame, int size,int value);
+void display_column_names(CDataframe frame, int size);
+void change_value_at(CDataframe frame, int size);
+
+//--------- Analysis and statistics -----
+void display_NumberOfColumns(CDataframe frame, int size);
+void display_NumberOfLines(CDataframe frame, int size);
+void values_equal_to_CD(CDataframe frame, int size, int x);
+
+
 
 #endif //PROJET_DATAFRAME_CDATAFRAME_H
